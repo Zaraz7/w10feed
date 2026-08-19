d6a8056793ece91068e53b3a743ad93a65d593c4
Zaraz7 <zaraz7@w10.site>
2026-08-19 17:15:48 +0300

    Removed dead code

--- src/w10feed/w10feed.py
+++ src/w10feed/w10feed.py
@@ -264,17 +264,7 @@ def create_blog_items(blog_posts, site_url, no_description=False):
                 description = re.sub(r'<h3[^>]*>.*?</h3>', '', content, count=1, flags=re.IGNORECASE | re.DOTALL)
             else:
                 description = content
-            # replace relative path to absolute
-            # description = re.sub(
-            #     r'href=(["\'])([^/][^"\']*)',
-            #     rf'href=\1{site_url}/cgi-bin/cms/\2',
-            #     description
-            # )
-            # description = re.sub(
-            #     r'=(["\'])\.\.\/\.\.\/',
-            #     rf'=\1{site_url}/',
-            #     description
-            # )
+
             description = replace_relative_urls(description, f'{site_url}/cgi-bin/cms/')
 
         
