4af54047ad9308ec08f574bc2bab63146891fe34
Zaraz7 <zaraz7@w10.site>
2026-08-19 12:17:31 +0300

    Added config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+# Config file of W10Feed
+#   
+# Example config:
+
+#PROFILES={
+#   "myblog":{
+#       "type":"blog",
+#       "host":"ftp.w10.host",
+#       "user":"<FTP user name here>",
+#       "passwd":"<FTP user password here>",
+#       "url":"https://example.w10.site",
+#       "lang":"en-EN",
+#       "title":"Title",
+#       "out":"blog.xml",
+#       "maxitems":50
+#   }
+#}
